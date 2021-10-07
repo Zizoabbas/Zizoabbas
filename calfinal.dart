737f0264e947c7c4d0af0f9515fe6556c4fa53a4
Zizoabbas <91802306+Zizoabbas@users.noreply.github.com>
2021-10-07 16:04:41 -0700

    Add files via upload

---
+++ calfinal.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+class Cal {
+  int add(num, num2) {
+    int c = num + num2;
+
+    return c;
+  }
+
+  int sub(num, num2) {
+    int c = num - num2;
+    return c;
+  }
+
+  int multi(num, num2) {
+    int c = num * num2;
+    return c;
+  }
+
+  double divi(num, num2) {
+    double c = num / num2;
+    return c;
+  }
+}
+
+void main(List<String> args) {
+  var cal2 = Cal();
+  print("calculator");
+  print("pliz inter first number");
+  var f = int.parse(stdin.readLineSync()!);
+  print("pliz enter the operation u need to +,-,/,*");
+  String h = stdin.readLineSync()!;
+  print("pliz enter second number");
+  var g = int.parse(stdin.readLineSync()!);
+  switch (h) {
+    case "+":
+      {
+        print(cal2.add(f, g));
+      }
+      break;
+    case "-":
+      {
+        print(cal2.sub(f, g));
+      }
+      break;
+    case "*":
+      {
+        print(cal2.multi(f, g));
+      }
+      break;
+    case "/":
+      {
+        try {
+          cal2.divi(f, 0);
+        } on IntegerDivisionByZeroException {
+          print("division by zero");
+        }
+        cal2.divi(f, g);
+        if (g == 0) {
+          throw new IntegerDivisionByZeroException();
+        }
+
+        print(cal2.divi(f, g));
+      }
+  }
+}
